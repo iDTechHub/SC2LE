17e5e550acd32bb6db7bd9dac2266cf92facef57
iD Tech <campsoftware@idsummerstaff.com>
2017-11-02 15:31:54 -0700

    Update selection_methods.py
    Fixing some wonkiness with selecting from a point.

--- selection_methods.py
+++ selection_methods.py
@@ -7,17 +7,14 @@ _NO_OP = actions.FUNCTIONS.no_op.id
 _SELECT_ARMY = actions.FUNCTIONS.select_army.id
 
 _SELECT_ALL = [0]
+_SCREEN = [0]
 
 # Reference https://github.com/deepmind/pysc2/blob/master/pysc2/lib/actions.py
-_MOVE_SCREEN = 331 #actions.FUNCTIONS.Move_screen.id 
+_MOVE_SCREEN = actions.FUNCTIONS.Move_screen.id 
 
-###
-# _SELECT_POINT = 2 #writing: actions.FUNCTIONS.select_point.id # would be more sensible
-# If you are selecting a specific unit, you should find it's unit id.
-# Those can be found at: https://github.com/Blizzard/s2client-api/blob/master/include/sc2api/sc2_typeenums.h
-# For instance a zergling is 105, a marine is 48
-# _ZERG_ZERGLING = 105
-###
+_SELECT_POINT = 2 #writing: actions.FUNCTIONS.select_point.id # would be more sensible
+_TERRAN_MARINE = 48
+_ZERG_ZERGLING = 105
 
 class ScriptedTechBot(base_agent.BaseAgent):
 
@@ -28,6 +25,8 @@ class ScriptedTechBot(base_agent.BaseAgent):
         # you must have a unit selected that you can move, otherwise, you don't 
         # have _MOVE_SCREEN in the available action space.
         if _MOVE_SCREEN in obs.observation["available_actions"]:
+            
+        	
             return actions.FunctionCall(_NO_OP, [])
             
         else:
@@ -36,21 +35,19 @@ class ScriptedTechBot(base_agent.BaseAgent):
             # It just selects all your friendly units, so no math or pointing, or rectangles. 
             # Drawbacks include: maybe you don't want to move your entire army every time. 
             return actions.FunctionCall(_SELECT_ARMY, [_SELECT_ALL])
+
+            # Then if you want to select a single point, then uncomment the following.
+            #unit_type = obs.observation["screen"][features.SCREEN_FEATURES.unit_type.index]
+            #zergling_y, zergling_x = (unit_type == _ZERG_ZERGLING).nonzero()
+
+            # The positional array should be averaged in 9 unit groupings to find a single 
+            # unit. so:
+            # [10:] in this case would be the second zergling.
+            # [:9] would be the first zergling. Although for some reason the second is more 
+            # reliable. I'm not sure why.
+            #select_x_mean = int(zergling_x[10:].mean())
+            #select_y_mean = int(zergling_y[10:].mean())
             
-            
-            ###
-            # Let's say you want to select a single unit. You could try:
-            # zergling_unit_type = obs.observation["screen"][features.SCREEN_FEATURES.unit_type.index] 
-            # zergling_y, zergling_x = (zergling_unit_type == _ZERG_ZERGLING).nonzero()
-            # On a map with two zerglings zergling_x could provide:
-            # print(zergling_x)
-            # [72 73 74 72 73 74 72 73 74 19 20 21 19 20 21 19 20 21] 
-            # print(zergling_y)
-            # [ 3  3  3  4  4  4  5  5  5  8  8  8  9  9  9 10 10 10]
-            # So the first ~9 elements are the 1st unit, and the second 
-            # set of elements is the second unit. So if you want to select 
-            # a point that is the second unit you could reference unit 10
-            # or higher in your x/y arrays like:
-            # select_point = [zergling_x[10], zergling_y[10]]
-            # return actions.FunctionCall(_SELECT_POINT, [[0], select_point]
-            ###
+            #select_point = [select_x_mean, select_y_mean]
+                
+            #return actions.FunctionCall(_SELECT_POINT, [_SCREEN, select_point])
